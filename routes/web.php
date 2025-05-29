c1d3ebb33458a5a0e94c0593688e46f71bc1005f
Zahran Syafiq <zahranae14@gmail.com>
2025-05-29 14:42:04 +0700

    Coba Pertama

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pesan_barang', function () {
+    return view('pesan_barang');
+});
